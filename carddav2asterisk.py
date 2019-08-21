814d74ef9fcfe7a2c3584b0cf036eac6cedfa2cb
Tristan Hill <tristan@me.uk.eu.org>
2019-08-21 08:58:15 +0100

    python3 compatibilty, use ini file for settings, remove prefix removal feature

--- carddav2asterisk.py
+++ carddav2asterisk.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
 import sys
@@ -8,53 +8,65 @@ import re
 from asterisk import manager
 from requests.auth import HTTPBasicAuth
 from lxml import etree
-from urlparse import urlparse
-
-# ASTERISK MANAGER CONNECTION
-HOST = 'localhost'
-PORT = 5038
-USER = 'carddavimport'
-PASS = 'cidpwd'
-
-# PERSONAL SETTINGS
-NATIONALPREFIX = "0049"
-DOMESTICPREFIX = "0841"
+from urllib.parse import urlparse
 
 # get list with links to all available vcards
 def getAllVcardLinks(url,auth):
   baseurl = urlparse(url).scheme+'://'+urlparse(url).netloc
   r = requests.request('PROPFIND',url,auth=auth)
-  root = etree.XML(r.content)
+  if r.status_code != 207:
+    raise RuntimeError('error in response from %s: %r' % (url, r))
+  root = etree.XML(r.text)
   vcardUrlList=[]
   for record in root.xpath(".//d:response",namespaces={"d" : "DAV:"}):
     type = record.xpath(".//d:getcontenttype",namespaces={"d" : "DAV:"})
     if (type) and type[0].text.startswith("text/vcard"):
       vcardlinks = record.xpath(".//d:href",namespaces={"d" : "DAV:"})
       for link in vcardlinks:
-        vcardUrlList.append(baseurl + '/' + link.text);
+        vcardUrlList.append(baseurl + link.text)
   return vcardUrlList
+
 
 def tidyPhoneNumber(num):
   num = re.sub("^\+","00",num)	# +39 -> 0039
   num = re.sub("\D","",num)		# remove all non-digits
-  num = re.sub("^"+NATIONALPREFIX+"0*","0",num)	# strip own national prefix
-  num = re.sub("^[^0]","0"+DOMESTICPREFIX,num)	# add domestic prefix, if missing
-
   return num
 
-def main(argv):
-  auth = HTTPBasicAuth(argv[2],argv[3])
-  url = argv[1]
+
+class NoManager(object):
+
+  def connect(self, host):
+    pass
+
+  def login(self, user, password):
+    pass
+
+  def send_action(self, data):
+    pass
+  
+  def logoff(self):
+    pass
+
+  def close(self):
+    pass
+
+
+def main(args, config):
+  auth = HTTPBasicAuth(config['carddav']['user'], config['carddav']['pass'])
+  url = config['carddav']['url']
 
   # connect to asterisk
-  ami = manager.Manager()
-  ami.connect(HOST)
-  ami.login(USER,PASS)
+  if args.no_update:
+    ami = NoManager()
+  else:    
+    ami = manager.Manager()
+  ami.connect(config['ami']['host'])
+  ami.login(config['ami']['user'], config['ami']['pass'])
 
   # get phone numbers from vcard
   for vurl in getAllVcardLinks(url,auth):
     r = requests.request("GET",vurl,auth=auth)
-    vcard = vobject.readOne(r.content)
+    vcard = vobject.readOne(r.text)
     if "tel" in vcard.contents:
       for telno in vcard.contents['tel']:
         num = tidyPhoneNumber(telno.value)
@@ -66,9 +78,13 @@ def main(argv):
   ami.close()
 
 if __name__ == "__main__":
-  if len(sys.argv)!=4:
-    print("Must be called with three arguments: <carddav-url> <carddav-user> <carddav-password>")
-    print("Example: %s https://owncloud.example.com/remote.php/dav/addressbooks/users/russmeyer/contacts/ meyerr p8a55w0rd" % sys.argv[0])
-    sys.exit(1)
-  sys.exit(main(sys.argv))
+  import argparse
+  parser = argparse.ArgumentParser()
+  parser.add_argument('ini_file')
+  parser.add_argument("--no-update", help="Don't call asterisk", action='store_true', default=False)
+  args = parser.parse_args()
+  import configparser
+  config = configparser.RawConfigParser()
+  config.read(args.ini_file)
+  main(args, config)
 
